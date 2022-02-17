13cd49d3321677439ede7db420d565bda3d9b99c
chen ran <lorrer@126.com>
2022-02-17 14:34:55 +0800

    更新example

--- app/src/main/java/com/loror/example/App.java
+++ app/src/main/java/com/loror/example/App.java
@@ -2,7 +2,9 @@ package com.loror.example;
 
 import android.app.Application;
 import android.content.Intent;
+import android.os.Build;
 
+import com.loror.debuger.DebugConfig;
 import com.loror.debuger.utils.BLog;
 import com.loror.debuger.CrashHandler;
 import com.loror.debuger.utils.SensorManagerUtil;
@@ -15,14 +17,22 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        DebugConfig.setDevice(Build.DEVICE);
+        DebugConfig.setSdk(Build.VERSION.SDK_INT);
+        DebugConfig.setVersion(BuildConfig.VERSION_NAME);
+        DebugConfig.setAllowRemote(true);
         File dir = getExternalCacheDir();
         if (dir != null) {
-            BLog.setSaveDir(dir.getAbsolutePath() + File.separator + "crash" + File.separator);
+            File debug = new File(dir, "cache/debug");
+            if (!debug.exists()) {
+                debug.mkdirs();
+            }
+            DebugConfig.setSaveDir(debug.getAbsolutePath());
             CrashHandler.getInstance().init(this);
         }
         SensorManagerUtil sensor = new SensorManagerUtil(this);
         sensor.setOnShakeListener(() -> {
-            startService(new Intent(this, DebugService.class));
+            DebugService.showIcon(this);
             sensor.stop();
         });
         sensor.start();
--- app/src/main/java/com/loror/example/MainActivity.java
+++ app/src/main/java/com/loror/example/MainActivity.java
@@ -1,8 +1,11 @@
 package com.loror.example;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.loror.debuger.DebugService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startService(new Intent(this, DebugService.class));
+        findViewById(R.id.text).setOnClickListener(v -> {
+            DebugService.showIcon(this);
+        });
     }
 }
